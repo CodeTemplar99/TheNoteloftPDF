9861f097f698da1fd3c51ed8a705912360307eca
Chiadika <udoagwac@gmail.com>
2020-04-06 14:36:10 +0100

    added slide

--- js/spine.js
+++ js/spine.js
@@ -16,3 +16,16 @@ function startSearch() {
 
     }
 }
+
+
+// slide
+var on = true;
+var myVar = setInterval(function () { if (on) { slides() } on = true; }, 7000);
+
+function slides() {
+    document.getElementsByTagName("img")[0].style.width = "0px";
+    document.getElementsByTagName("img")[1].style.width = "100%";
+    var slide = document.getElementsByTagName("img")[0];
+    document.getElementById("slide").appendChild(slide);
+
+}
